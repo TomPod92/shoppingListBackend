02c3b08f898ff43f7a1e723ec479a886fea46ba7
Tomasz Podsiadlik <tpodsiadlik@future-processing.com>
2020-06-01 12:25:23 +0200

    Create endpoint for creating a product

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ require('dotenv').config();
 
 const express = require('express');
 const connectDB = require('./dbConfig.js');
+
 const User = require('./models/User.model');
+const Product = require('./models/Product.model');
+
 const app = express();
 
 // Połączenie z baza danych
@@ -14,19 +17,29 @@ app.use(express.json({extended: false}));
 const PORT = process.env.PORT || 5000;
 app.get('/', (req, res) => res.send('Shopping List API running...'));
 
-
+//-------------------------------------------------------
 app.post('/users', async (req, res) => {
-    console.log('body: ', req.body)
     const user = new User(req.body);
 
     try {
         await user.save();
-        res.send(user);
+        res.status(201).send(user);
     } catch (error) {
         console.error(error);
         res.status(400).send(error)
     }
 });
+//-------------------------------------------------------
+app.post('/products', async (req, res) => {
+    const product = new Product(req.body);
 
+    try {
+        await product.save();
+        res.status(201).send(product);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+//-------------------------------------------------------
 
 app.listen(PORT, () => console.log(`Server for Shopping List started on port ${PORT}`));
--- models/Product.model.js
+++ models/Product.model.js
@@ -2,11 +2,16 @@ const mongoose = require('mongoose');
 
 const Product = mongoose.model('Product', {
     section: {
-        type: String
+        type: String,
+        required: true
     },
     name: {
-        type: String
+        type: String,
+        required: true
     },
+    shops: {
+        type: Array
+    }
 });
 
 module.exports = Product;
